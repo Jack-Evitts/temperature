bb759ae2eb0fe3db470ecf8923682fc5e4eea290
Samuel Skirvin <samuel.skirvin@gmail.com>
2018-09-05 12:30:29 +0100

    I fixed it

--- test_conversion.py
+++ test_conversion.py
@@ -3,7 +3,3 @@ import conversion
 def test_kelvin_to_celsius():
 <<<<<<< HEAD
     assert conversion.kelvin_to_celsius(0) == -273.15
-
-=======
-    assert conversion.kelvin_to_celsius(0) == -273.15
->>>>>>> fc93e11bc8b61a3d56eb1e196ebcbf359d7923a0
