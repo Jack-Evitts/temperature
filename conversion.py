63b4f6ff285532b59563a447eea11972429599e5
Jack Evitts <jje9@kent.ac.uk>
2018-09-05 11:14:06 +0100

    Add initial imlementation of temperature conversion.

---
+++ conversion.py
@@ -0,0 +1,10 @@
+def kevin_to_celsius(temp):
+    return temp - 273.15
+
+def celsius_to_fahrenheit(temp):
+    return temp * (9/5) + 32
+
+def kelvin_to_fahrenheit(temp):
+    temp_c = kelvin_to_celsius(temp)
+    result = celsius_to_fahrenheit(temp_c)
+    return result
